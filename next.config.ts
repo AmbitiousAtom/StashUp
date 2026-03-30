b4ffc458eabd813f02c7c8720d24f6505c461ee5
Jaspreet1224 <jsingh44@buffalo.edu>
2026-03-30 11:47:24 -0400

    Configure project for Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = "StashUp";
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  ...(isGithubPages
-    ? {
-        basePath: `/${repoName}`,
-        assetPrefix: `/${repoName}/`,
-      }
-    : {}),
 };
 
 export default nextConfig;
